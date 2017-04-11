238962ef44b1c04e8931494a61032dd6e3d31163
Jamie Scott <github@jami.org.uk>
2017-04-11 15:24:39 +0100

    Added more field to RecordDetails for future expandability

--- domain/src/main/java/com/uk/mycompany/domain/RecordDetails.java
+++ domain/src/main/java/com/uk/mycompany/domain/RecordDetails.java
@@ -1,8 +1,16 @@
 package com.uk.mycompany.domain;
 
+import java.util.List;
+
 public class RecordDetails extends AbstractDomainObject {
 
     private String username;
+    private String forename;
+    private String surname;
+    private String gender;
+    private String emailAddress;
+    private String reasonAttending;
+    private List<String> interests;
     private String timestamp;
     private String status;
     private String location;
@@ -46,5 +54,53 @@ public class RecordDetails extends AbstractDomainObject {
 
     public void setLocation(final String location) {
         this.location = location;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getReasonAttending() {
+        return reasonAttending;
+    }
+
+    public void setReasonAttending(String reasonAttending) {
+        this.reasonAttending = reasonAttending;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
     }
 }
